290ae63d2aaa8121f3082caadef3bd3b74a11b39
jugjug7531 <24523617+jugjug7531@users.noreply.github.com>
2022-05-26 21:42:22 +0900

    メッセージ削除機能追加

--- lib/chat/component/add_post_list.dart
+++ lib/chat/component/add_post_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:provider/provider.dart';
+import 'package:chat_app/login/model/user_login_model.dart';
 
 /// 投稿メッセージ一覧リスト
 /// 
@@ -9,6 +11,9 @@ class AddPostList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
+    // 投稿メッセージモデル取得
+    final userLoginModel = Provider.of<UserLoginModel>(context, listen:false);
+
     return  FutureBuilder<QuerySnapshot>(
       // 投稿メッセージ一覧を取得（非同期処理）
       // 投稿日時でソート
@@ -27,6 +32,19 @@ class AddPostList extends StatelessWidget {
                 child: ListTile(
                   title: Text(document['text']),
                   subtitle: Text(document['email']),
+                  // 自分の投稿メッセージの場合は削除ボタンを表示
+                  trailing: document['email'] == userLoginModel.email
+                      ? IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () async {
+                            // 投稿メッセージのドキュメントを削除
+                            await FirebaseFirestore.instance
+                                .collection('posts')
+                                .doc(document.id)
+                                .delete();
+                          },
+                        )
+                      : null,
                 ),
               );
             }).toList(),
